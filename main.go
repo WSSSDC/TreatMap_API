1453074043c45e6ae315c6bd9141fc3dfb089488
Bram <ogus.bram@gmail.com>
2021-10-29 23:54:27 -0400

    Removed Tests

--- main.go
+++ main.go
@@ -3,9 +3,6 @@ package main
 import (
 	"context"
 	"log"
-	data_api "treatMap/trick-or-treat/src/data"
-	"treatMap/trick-or-treat/src/structs"
-	s "treatMap/trick-or-treat/src/structs"
 
 	firebase "firebase.google.com/go"
 	"google.golang.org/api/option"
@@ -21,16 +18,5 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-	test_user := s.User{
-		UUID:     "test",
-		Username: "Bramogus",
-		Points:   100,
-		Reports:  100,
-		Ranking:  100,
-	}
-	var user structs.User
-	data_api.Set_User(test_user, client)
-	user = data_api.Get_User(test_user.UUID, client)
-	log.Print(user)
 	client.Close()
 }
